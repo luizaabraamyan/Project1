575a9a6cd88dbc20478f90015fa4cf988fece045
luizaabraamyan <lusika_a@abv.bg>
2015-12-13 19:25:55 +0200

    Update Task1Demo.java

--- src/Task1Demo.java
+++ src/Task1Demo.java
@@ -2,7 +2,28 @@
 public class Task1Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		char[][] matrix = { { 'A', 'I', 'O', 'C', 'P' }, { 'N', 'T', 'E', 'J', 'M' }, { 'D', 'K', 'U', 'G', 'S' },
+				{ 'W', 'H', 'R', 'L', 'Y' }, { 'F', 'Q', 'B', 'V', 'X' } };
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter some text: ");
+		String text = input.next();
+		String upperText;
+
+		upperText = text.toUpperCase();
+		int len = upperText.length();
+
+		if (upperText.length() % 2 != 0) {
+			upperText = upperText + "A";
+		}
+
+		String getEncryptedText = encryption(matrix, upperText);
+		System.out.println("Crypted text: " + getEncryptedText);
+
+		String getDecryptedText = decryption(matrix, getEncryptedText, len);
+		System.out.println("Decrypted text: " + getDecryptedText);
+
+		input.close();
 
 	}
 
